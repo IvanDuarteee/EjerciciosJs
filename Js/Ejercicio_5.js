c6aaecb92e7fceec3473ce93770f8243fb58fa7f
Walter Ivan Duarte <wduarte@noanet.com.ar>
2023-02-27 09:28:47 -0300

    Se agrega ejercicio_5

---
+++ Js/Ejercicio_5.js
@@ -0,0 +1,7 @@
+/* Ejercicio N_5 */
+
+let num1 = parseInt(prompt("¡Realizá tu suma!","Ingresa el primer numero"));
+
+let num2 = parseInt(prompt("¡Realizá tu suma!","Ingresa el segundo numero"));
+
+alert("El resultado de la suma es: " + (num1+num2))
