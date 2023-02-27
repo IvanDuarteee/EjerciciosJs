72d7fe9c1c429049825f91a53baee7a7719c7b18
Walter Ivan Duarte <wduarte@noanet.com.ar>
2023-02-27 12:00:33 -0300

    Se agrega igualdad de numeros

--- Js/Ejercicio_6.js
+++ Js/Ejercicio_6.js
@@ -8,13 +8,19 @@ let num1 = parseInt(prompt("¿Qué número es mayor?","Ingresa el primer numero"
 let num2 = parseInt(prompt("¿Qué número es mayor?","Ingresa el segundo numero"));
 
 if(num1>num2){
-    valor = num1
-    //("El numero mayor es: " + num1);
+    valor = num1;
 }
 else{
-    valor = num2
-    //("El numero mayor es: " + num2);
+    valor = num2;
+}
+ 
+if (num1>num2){
+    alert("El numero mayor es: " + valor);
+}
+
+else {
+    alert ("Los numeros son iguales");
 }
 
 
-alert("El numero mayor es: " + valor)
+
