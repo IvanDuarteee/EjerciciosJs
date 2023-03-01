694ebf52c37134915f6738d8d69f7e91bfbef860
Walter Ivan Duarte <wduarte@noanet.com.ar>
2023-03-01 09:41:24 -0300

    Se realiza ejercicio_8

---
+++ Js/Ejercicio_8.js
@@ -0,0 +1,16 @@
+// Ejercicio Nº8
+
+// Escribe un programa que escriba un numero y este sea divisible por dos.
+
+//let num = 0 ;
+
+let num1 = parseInt(prompt("Ingrese el numero aqui"))
+
+if (num1%2 === 0) {
+     alert("Este numero es divisible por dos")
+}
+
+else {
+    alert( "Este numero no es divisible por dos")
+}
+
