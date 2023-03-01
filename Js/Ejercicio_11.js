fcb1a2e0d34e11b662d1deba78c8cb214e064cb5
Walter Ivan Duarte <wduarte@noanet.com.ar>
2023-03-01 12:11:57 -0300

    Se actualizan ejercicios

--- Js/Ejercicio_11.js
+++ Js/Ejercicio_11.js
@@ -2,19 +2,12 @@
 
 //Añadir al ejercicio anterior que nos diga por cuál de los cuatro es divisible (hay que decir todos por los que es divisible)
 
-
-let num = prompt(
-    "Ingresá un número y verificá si es divisible por: 2,3,5 o 7",
-    "Ingresá el número aquí"
-  );
-  if (num % 2 === 0) {
-    alert("Es divisible por 2");
-  } else if (num % 3 === 0 ) {
-    alert("Es divisible por 3");
-  } else if (num % 5 === 0) {
-    alert("Es divisible por 5");
-  } else if (num % 7 === 0) {
-    alert("Es divisible por 7");
+let divisores = [2, 3, 5, 7];
+let num = parseInt(prompt("Ingrese el número: "));
+for (i = 0; i < divisores.length; i++) {
+  if (num % divisores[i] == 0) {
+    document.write("Es divisible en " + divisores[i] + "<br><br>");
   } else {
-    alert("No es divisble por ninguno de éstos números");
+    document.write("No es divisible en " + divisores[i] + "<br><br>");
   }
+}
